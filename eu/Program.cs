aecb89e50a544dcbf4d645b51d59f426b479612c
Bdani2201 <bdani2201@gmail.com>
2022-01-19 11:54:05 +0100

    Add project files.

---
+++ eu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eu
+{
+    class Unioallamai
+    {
+        public Unioallamai(string sor)
+        {
+            string[] sorelemek = sor.Split(';');
+            this.Orszag = sorelemek[0];
+            this.Datum = Convert.ToDateTime(sorelemek[1]);
+
+        }
+
+        public string Orszag { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
+    
